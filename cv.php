ab799dd8f2ca40a77eb72a27a8fa6aa622b8f29b
Jberestian <jberestian@gmail.com>
2024-12-13 11:37:04 +0200

    added changes in cv img

--- cv.php
+++ cv.php
@@ -13,8 +13,8 @@
     <main>
       <section id="etat-civil">
         <h1 class="etat-civil-title">Etat Civil</h1>
-        <img src="./images/maPhoto.png" />
-        <p>Jane Doe</p>
+        <img src="./images/photos/image-principale.avif" class="etat-civil-img" />
+        <p class="etat-civil-text">Jane Doe</p>
         <p>20 place de l'Eglise</p>
         <p>38000, GRENOBLE</p>
         <p>+336xxxxxx</p>
@@ -23,7 +23,7 @@
       <section id="formation">
         <h1 class="formation-title">Formation</h1>
         <dl>
-          <dt>Master Spécialité Compétence Complémentaire en Informatique</dt>
+          <dt class="formation-text">Master Spécialité Compétence Complémentaire en Informatique</dt>
           <dd>Université Joseph Fourier 2010–2012</dd>
           <dt>Master Spécialité XXXX</dt>
           <dd>Université YYY 2008–2010</dd>
@@ -34,7 +34,7 @@
       <section id="competences">
         <h1 class="competences-title">Compétences Informatique</h1>
         <dl>
-          <dt>Langages de Programmation</dt>
+          <dt class="competences-text">Langages de Programmation</dt>
           <dd>Java, Swift, C++, Python, Scala</dd>
           <dt>Technologies du web</dt>
           <dd>HTML, CSS, JavaScript, PHP, technologies XML</dd>
@@ -48,7 +48,7 @@
       </section>
       <section id="experiences">
         <h1 class="experiences-title">Experiences profesionnelles</h1>
-        <p>à compléter.....</p>
+        <p class="experiences-text">à compléter.....</p>
       </section>
     </main>
     <?php include 'footer.php'; ?>
